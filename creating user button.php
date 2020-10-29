4cec7b484b1d2b9ea183130554468c92b9306a9e
Jacob Lapkin <65730020+Post-Shalom@users.noreply.github.com>
2020-10-29 11:40:15 -0600

    Update creating user button.php

--- creating user button.php
+++ creating user button.php
@@ -6,11 +6,11 @@
 </head>
 <body>
 <form method="POST">
-	<input type="text" name="userident" id="UID">
-	<input type="text" name="First_name" id="FN">
-	<input type="text" name="Last_name" id-"LN">
-	<input type="email" name="email" id="E-mail">
-	<input type="password" name="password" id="pwd">
+	<input type="text" name="userident" placeholder="USERID">
+	<input type="text" name="First_name" placeholder="firstname">
+	<input type="text" name="Last_name" placeholder="lastname">
+	<input type="email" name="email" placeholder="E-mail">
+	<input type="password" name="password" placeholder="Password">
 	
 	<button type="submit" name="submit">sign up</button>
 	
