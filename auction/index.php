7609487669b70bed11e63fd040c6443b0a87bb93
Alex Liu <alex1029453257@icloud.com>
2020-11-13 18:55:34 +0800

    Fixed some bugs and rolled back some changes in login_result and index.php

--- auction/index.php
+++ auction/index.php
@@ -2,13 +2,15 @@
   // For now, index.php just redirects to browse.php, but you can change this
   // if you like.
   session_start();
-  if(!isset($_SESSION) {
+  if(!isset($_SESSION['logged_in'])) {
     echo "Redirecting to browse page";
     header("refresh:3;url=browse.php");}
+  else {
   if ($_SESSION['account_type'] == 'buyer') {
     echo "Redirecting to browse page";
     header("refresh:3;url=browse.php");}
   if ($_SESSION['account_type'] == 'seller'){
     echo "Redirecting to list page";
     header("refresh:3;url=mylistings.php");}
+  }
 ?>
--- auction/login_result.php
+++ auction/login_result.php
@@ -16,29 +16,30 @@ $check_result = mysqli_query($connection,$check_query);
 $check = mysqli_fetch_array($check_result);                     // Make query to the database and fetch stored user information
 
 if (!$check){ echo('<div class="text-center">Login Failed. Email does not exist or Password Incorrect.</div>'); } // Invalid input, Login Failed
-
 else {  
-$_SESSION['logged_in'] = true;
-$_SESSION['user_id'] = $check['user_id'];
 
-$seller_query = "SELECT seller_id FROM sellers WHERE user_id = $check['user_id']";
-$seller_result = mysqli_query($connection,$seller_query);
-$seller = mysqli_fetch_array($seller_result);
-if($seller_result) {$_SESSION['account_type'] = 'seller';
-$_SESSION['seller_id']=$seller['seller_id'];}  // Seller ID in session
+    $_SESSION['logged_in'] = true;
+    $userid = $check['user_id'];
+    $_SESSION['user_id'] = $check['user_id'];
+    $seller_query = " SELECT seller_id FROM sellers WHERE user_id=$userid; ";
+    $seller_result = mysqli_query($connection,$seller_query);
+    $seller = mysqli_fetch_array($seller_result);
+    if($seller_result) {$_SESSION['account_type'] = 'seller';
+    $_SESSION['seller_id']=$seller['seller_id'];}  // Seller ID in session
 
-$buyer_query = "SELECT buyer_id FROM buyers WHERE user_id = $check['user_id']";
-$buyer_result = mysqli_query($connection,$buyer_query);
-$buyer = mysqli_fetch_array($seller_result);
-if($buyer_result) {$_SESSION['account_type'] = 'buyer';
-$_SESSION['buyer_id']=$buyer['buyer_id'];}  // Buyer ID in session
+    $buyer_query = " SELECT * FROM buyers WHERE user_id = $userid ";
+    $buyer_result = mysqli_query($connection,$buyer_query);
+    $buyer = mysqli_fetch_array($buyer_result);
+    if($buyer_result) {$_SESSION['account_type'] = 'buyer';
+    $_SESSION['buyer_id']=$buyer['buyer_id'];}  // Buyer ID in session
 
-if(!isset($_SESSION[account_type])) {$_SESSION['account_type']= 'visitor'}   // To Andrew: Defined session's account types and IDs for your usage.
+    if(!isset($_SESSION['account_type'])) {$_SESSION['account_type']= 'visitor';}  
+// To Andrew: Defined session's account types and IDs for your usage.
 
 // Set session name and account type.
-echo('<div class="text-center">You are now logged in as: '. $_SESSION['account_type'] . ', You will be redirected shortly.</div>');
+    echo('<div class="text-center">You are now logged in as: '. $_SESSION['account_type'] . ', You will be redirected shortly.</div>');
 // Redirect to index after 5 seconds
-header("refresh:5;url=index.php");
+    header("refresh:5;url=index.php");
 }
 
 mysqli_close($connection);
