e7e8e9863328f3fafc117aa155427a6fbb37d201
Post Shalom <jacobglapkin@gmail.com>
2020-11-21 18:16:39 +0000

    Update process_registration.php
    fixed email input and couple other logical errors in registering

--- auction/process_registration.php
+++ auction/process_registration.php
@@ -17,7 +17,11 @@ else{
 }
 
 //defining POST variables
-$email = $_POST['email'];
+$email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+
+
+
+
 $Password = $_POST['password'];
 $repeat_password = $_POST['passwordrepeat'];
 $accounttype = $_POST['accountType'];
@@ -39,14 +43,19 @@ $emailquery = ("SELECT * FROM users WHERE email = '$email'");
 $emailresult = mysqli_query($connect, $emailquery);
 $emailcheck = mysqli_num_rows($emailresult)>0;
 if($emailcheck){
-	echo " Email already registered.";
+	echo " Email already registered. ";
 	header('Refresh:3, url=browse.php'); //add sessions later
 }
 else{
-if ($Password == $repeat_password){
-$result = mysqli_query($connect,$query)
-	or die(" insert into database unsuccessfull");
-	header('Refresh:3, url=browse.php'); //add sessions later.
+if (filter_var($email, FILTER_VALIDATE_EMAIL)){
+	if ($Password == $repeat_password and $Password != ''){
+		$result = mysqli_query($connect,$query)
+		or die(" insert into database unsuccessfull ");
+		header('Refresh:3, url=browse.php'); //add sessions later.
+}
+}
+else{
+	echo " Email is not valid. ";
 }
 }
 /*only inserting data if the password and password repeat match. 
@@ -56,14 +65,15 @@ noting to user that information did not insert */
 
 //if user registers as buyer then insert id into buyers table...auto incrementing buyer id
 
-
-if ($accounttype == "buyer"){
-$buyerresult = mysqli_query($connect, $buyerquery);
+if ($result){
+	if ($accounttype == "buyer"){
+		$buyerresult = mysqli_query($connect, $buyerquery);
 }
 
 //if user registers as seller then insert id into sellers table..auto incrementing seller id
-if ($accounttype == "seller"){
-$sellerresult = mysqli_query($connect, $sellerquery);
+	if ($accounttype == "seller"){
+		$sellerresult = mysqli_query($connect, $sellerquery);
+}
 }
 
 
@@ -74,15 +84,15 @@ $sellerresult = mysqli_query($connect, $sellerquery);
 
 if ($result)
 {
-	echo " Registered successfully. Registered with email: $email";
+	echo " Registered successfully. Registered with email: $email ";
 }
 else{
-	echo " registered unsuccessfully";
+	echo " Registered unsuccessfully. ";
 	header('Refresh:3; url=register.php'); //add sessions later.  
 }
 
 if ($Password != $repeat_password){
-	echo "passwords do not match";
+	echo " Passwords do not match ";
 }
 mysqli_close($connect);
 	
