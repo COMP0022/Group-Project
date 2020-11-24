1edaec831ec7216718b29519ee6dec5ecbf91ef1
Andrew O'Connell <Andrew O'Connell>
2020-11-24 20:55:40 +0000

    Update winner_email.php
    Updated query to select the email of winner bidder.

--- auction/Winner_email.php
+++ auction/Winner_email.php
@@ -1,9 +1,27 @@
 <?php
-$winner_email_query = "SELECT email FROM users WHERE id IN(
-    SELECT user_id FROM buyers WHERE buyer_id IN(
-        SELECT buyer_id FROM bids WHERE bid_id IN(
-            SELECT bid_id FROM bids WHERE listing_id = 1 AND bidprice IN(
-                SELECT MAX(bidprice) as bidprice FROM bids WHERE listing_id = $item_id))))";
+// $winner_email_query = "SELECT email FROM users WHERE id IN(
+//     SELECT user_id FROM buyers WHERE buyer_id IN(
+//         SELECT buyer_id FROM bids WHERE bid_id IN(
+//             SELECT bid_id FROM bids WHERE listing_id = 1 AND bidprice IN(
+//                 SELECT MAX(bidprice) as bidprice FROM bids WHERE listing_id = $item_id))))";
+
+$winner_email_query = "SELECT
+    email
+FROM
+    bids AS bi
+INNER JOIN buyers AS bu USING(buyer_id)
+INNER JOIN users AS u
+ON
+    bu.user_id = u.id
+WHERE
+    listing_id = 5 AND bidprice IN(
+    SELECT
+        MAX(bidprice)
+    FROM
+        bids
+    WHERE
+        listing_id = 5)"
+
 
 $listing_title_query = "SELECT item_title FROM listings WHERE listing_id = $item_id";
 
@@ -16,17 +34,17 @@ $seller_email_query = "SELECT email FROM users WHERE id IN(SELECT user_id FROM s
 
 $seller_email_result = mysqli_query($connection, $seller_email_query)
 	or die('Error making seller email query');
-	
+
 $seller_email = mysqli_fetch_array($seller_email_result);
 
 $title_result = mysqli_query($connection, $listing_title_query) or die('Error making listing title query');
-	
+
 $listing_title = mysqli_fetch_array($title_result);
 
 $top_bid_query = "SELECT MAX(bidprice) FROM bids WHERE listing_id = $item_id";
-			
+
 $top_bid_result = mysqli_query($connection, $top_bid_query)
-	or die('Error making top bid query');	
+	or die('Error making top bid query');
 
 $top_bid = mysqli_fetch_array($top_bid_result);
 
@@ -36,7 +54,7 @@ $recipient = "$winner_email[0]"; //recipient
 $mail_body = "Congratulations! You bid £$top_bid[0] on the auction called '$listing_title[0]'. This was the highest bid, and you have won!"; //mail body
 $subject = "Congratulations"; //subject
 $header = "From: ". $name ." <" . $email .">\r\n"; //optional headerfields
-mail($recipient, $subject,$mail_body ,$header); //mail function	
+mail($recipient,$subject,$mail_body,$header); //mail function
 
 
 $name = "Happy Auction House"; //sender’s name
@@ -45,7 +63,6 @@ $recipient = "$seller_email[0]"; //recipient
 $mail_body = "Congratulations! Your listing called '$listing_title[0]' has now finished. The winning bid was £$top_bid[0]"; //mail body
 $subject = "Auction finished"; //subject
 $header = "From: ". $name ." <" . $email .">\r\n"; //optional headerfields
-
-mail($recipient, $subject,$mail_body ,$header); //mail function
+mail($recipient,$subject,$mail_body,$header); //mail function
 
 ?>
