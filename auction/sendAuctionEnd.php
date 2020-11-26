2119d6a8e6728a75988ecedcebdf114af2ded5fb
Alex Liu <alex1029453257@icloud.com>
2020-11-26 18:28:20 +0000

    Added detect auction end function and mail sending

--- auction/sendAuctionEnd.php
+++ auction/sendAuctionEnd.php
@@ -104,3 +104,13 @@ function smtpmailer($to, $from, $from_name, $subject, $body)
 
 ?>
 
+
+<html>
+    <head>
+        <title>PHPMailer 5.2 testing from DomainRacer</title>
+    </head>
+    <body style="background: black;">
+        <center><h2 style="padding-top:70px;color: white;"><?php echo $error; ?></h2></center>
+    </body>
+
+</html>
