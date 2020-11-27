e6ccd47b72e447ccab24ac3be758f7e8163cb9d0
Alex Liu <alex1029453257@icloud.com>
2020-11-27 21:16:49 +0000

    Changed queries for new DB structure, and modified details of login, bid-placing and logout

--- auction/login_result.php
+++ auction/login_result.php
@@ -1,11 +1,11 @@
+<?php include_once("header.php")?>
+<?php require("utilities.php")?>
 <?php
 
 // TODO: Extract $_POST variables, check they're OK, and attempt to login.
 // Notify user of success/failure and redirect/give navigation options.
 
 // For now, I will just set session variables and redirect.
-
-session_start();
 
 $input_email = $_POST['email'];
 $input_password = $_POST['password'];                           // Retreive the form input from header.php
@@ -19,31 +19,20 @@ if (!$check)
 { 
     echo('<div class="text-center">Login Failed. Email does not exist or Password Incorrect.</div>'); 
     mysqli_close($connection);
-    header("refresh:2;url=index.php");
+    header("refresh:5;url=index.php");
 }                                                              // Invalid input, Login Failed, Return to browse page
 
 else 
 {  
-    $_SESSION['logged_in'] = true;                             // Login success, next, check seller and buyer id
+    $_SESSION['logged_in'] = true;                            
+    $_SESSION['user_id'] = $check['id'];
 
-    $userid = $check['id'];
-    $_SESSION['user_id'] = $check['id'];                       // Define the logged in user's user_id as SESSION variable for other uses
-
-    $seller_query = " SELECT seller_id FROM sellers WHERE user_id=$userid; ";
-    $seller_result = mysqli_query($connection,$seller_query);
-    $seller = mysqli_fetch_array($seller_result);
-    if($seller) {$_SESSION['account_type'] = 'seller';
-    $_SESSION['seller_id']=$seller['seller_id'];}              // Check if the logged in user was registered as a seller, if so, get its seller_id for other uses
-
-    $buyer_query = " SELECT * FROM buyers WHERE user_id = $userid ";
-    $buyer_result = mysqli_query($connection,$buyer_query);
-    $buyer = mysqli_fetch_array($buyer_result);
-    if($buyer) {$_SESSION['account_type'] = 'buyer';
-    $_SESSION['buyer_id']=$buyer['buyer_id'];}                 // Check if the logged in user was registered as a buyer, if so, get its buyer_id for other uses
+    if ($check['type']==1) {$_SESSION['account_type'] = 'seller';}
+    if ($check['type']==0) {$_SESSION['account_type'] = 'buyer';}
 
     if(!isset($_SESSION['account_type'])) {$_SESSION['account_type']= 'visitor';}  
-                                                               // If the user was not registered as buyer or seller, mark him/her as a visitor
-
+    
+    // If the user was not registered as buyer or seller, mark him/her as a visitor
 
     echo('<div class="text-center">You are now logged in as: '. $_SESSION['account_type'] . ', You will be redirected shortly.</div>');
     mysqli_close($connection);
