cb6dc007873ac9f7d0124429822b1a15b6879909
JiangFan95 <34374133+JiangFan95@users.noreply.github.com>
2020-11-30 09:31:28 +0000

    protecting for sql attacks

--- auction/browse.php
+++ auction/browse.php
@@ -77,7 +77,7 @@ FROM listings LEFT JOIN bids ON listings.listing_id=bids.listing_id WHERE item_t
 	else
 	{
 		// Sets keyword Variable
-		$keyword = $_GET['keyword'];
+		$keyword = htmlspecialchars(mysqli_real_escape_string($connection,$_GET['keyword']));
 		
 		// Checks if Keyword is blank
 		if ($keyword == '')
